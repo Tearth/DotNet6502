53788d92c9e96665be4c4f025bd8f9ef5dceab4c
tearth <tearthdev@gmail.com>
2020-03-31 21:01:29 +0200

    Add addressing modes enum

---
+++ Emulator/InstructionDecoder/AddressingMode.cs
@@ -0,0 +1,20 @@
+﻿namespace Emulator.Instructions
+{
+    public enum AddressingMode
+    {
+        None,
+        Implicit,
+        Accumulator,
+        Immediate,
+        ZeroPage,
+        ZeroPageX,
+        ZeroPageY,
+        Relative,
+        Absolute,
+        AbsoluteX,
+        AbsoluteY,
+        Indirect,
+        IndexedIndirect,
+        IndirectIndexed,
+    }
+}
