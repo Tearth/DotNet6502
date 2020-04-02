6b7af2186c28bf3e08a6538e77299b7c48eb5d91
tearth <tearthdev@gmail.com>
2020-04-02 19:55:30 +0200

    Add ROM device

---
+++ Peripherals/ROM/RomDevice.cs
@@ -0,0 +1,46 @@
+﻿using CPU.IO;
+
+namespace ROM
+{
+    public class RomDevice : IDevice
+    {
+        private readonly ushort _startAddress;
+        private readonly ushort _endAddress;
+        private ushort Size => (ushort)(_endAddress - _startAddress);
+
+        private readonly byte[] _memory;
+
+        public RomDevice(ushort startAddress, ushort endAddress)
+        {
+            _startAddress = startAddress;
+            _endAddress = endAddress;
+            _memory = new byte[Size];
+        }
+
+        public byte Read(ushort address)
+        {
+            if (IsAddressValid(address))
+            {
+                var relativeAddress = GetRelativeAddress(address);
+                return _memory[relativeAddress];
+            }
+
+            return 0;
+        }
+
+        public void Write(ushort address, byte value)
+        {
+
+        }
+
+        private bool IsAddressValid(ushort address)
+        {
+            return address >= _startAddress && address <= _endAddress;
+        }
+
+        private ushort GetRelativeAddress(ushort address)
+        {
+            return (ushort)(address - _startAddress);
+        }
+    }
+}
