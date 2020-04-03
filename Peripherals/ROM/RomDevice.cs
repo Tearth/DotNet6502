430a652f5ecd5146da2bb4a7a1891175bd4cc6fa
tearth <tearthdev@gmail.com>
2020-04-03 15:44:51 +0200

    Add better support for ROM configuration

--- Peripherals/ROM/RomDevice.cs
+++ Peripherals/ROM/RomDevice.cs
@@ -1,20 +1,65 @@
-﻿using CPU.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CPU.IO;
 
 namespace ROM
 {
     public class RomDevice : IDevice
     {
-        private readonly ushort _startAddress;
-        private readonly ushort _endAddress;
-        private ushort Size => (ushort)(_endAddress - _startAddress);
+        private ushort _startAddress;
+        private ushort _endAddress;
+        private uint Size => (uint)(_endAddress - _startAddress + 1);
 
-        private readonly byte[] _memory;
+        private byte[] _memory;
 
-        public RomDevice(ushort startAddress, ushort endAddress)
+        public bool Configure(List<string> parameters)
         {
-            _startAddress = startAddress;
-            _endAddress = endAddress;
+            if (parameters.Count < 2)
+            {
+                return false;
+            }
+
+            // Parse start address (required)
+            if (!ushort.TryParse(parameters[0].Replace("0x", ""), NumberStyles.HexNumber, null, out _startAddress))
+            {
+                return false;
+            }
+
+            // Parse end address (required)
+            if (!ushort.TryParse(parameters[1].Replace("0x", ""), NumberStyles.HexNumber, null, out _endAddress))
+            {
+                return false;
+            }
+
             _memory = new byte[Size];
+
+            // Parse image file (optional)
+            if (parameters.Count > 2)
+            {
+                var imagePath = parameters[2];
+                if (!File.Exists(imagePath))
+                {
+                    return false;
+                }
+
+                var atIndex = 0;
+
+                // Parse image start index (optional)
+                if (parameters.Count > 3)
+                {
+                    if (!int.TryParse(parameters[3].Replace("0x", ""), NumberStyles.HexNumber, null, out atIndex))
+                    {
+                        return false;
+                    }
+                }
+
+                var imageBytes = File.ReadAllBytes(imagePath);
+                Array.Copy(imageBytes, 0, _memory, atIndex, imageBytes.Length);
+            }
+
+            return true;
         }
 
         public byte Read(ushort address)
