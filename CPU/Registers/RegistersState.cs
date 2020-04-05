08456a8ebe34db2326dccd32c4e163185b7e8bca
tearth <tearthdev@gmail.com>
2020-04-05 14:46:55 +0200

    Add comments

--- CPU/Registers/RegistersState.cs
+++ CPU/Registers/RegistersState.cs
@@ -2,11 +2,34 @@
 {
     public class RegistersState
     {
+        /// <summary>
+        /// 16-bit register containing address of the executing instruction.
+        /// </summary>
         public ushort ProgramCounter { get; set; }
+
+        /// <summary>
+        /// 8-bit register containing address of the stack pointer.
+        /// </summary>
         public byte StackPointer { get; set; }
+
+        /// <summary>
+        /// 8-bit register used for arithmetic operations.
+        /// </summary>
         public byte Accumulator { get; set; }
+
+        /// <summary>
+        /// 8-bit register used in various memory addressing modes.
+        /// </summary>
         public byte IndexRegisterX { get; set; }
+
+        /// <summary>
+        /// 8-bit register used in various memory addressing modes.
+        /// </summary>
         public byte IndexRegisterY { get; set; }
+
+        /// <summary>
+        /// 8-bit register containing processor status.
+        /// </summary>
         public StatusFlags Flags { get; set; }
     }
 }
