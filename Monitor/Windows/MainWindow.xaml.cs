668ea456f9fb26b40d600dd2781cbe336f020e6c
tearth <tearthdev@gmail.com>
2020-04-06 13:51:36 +0200

    Improve menu bar

--- Monitor/Windows/MainWindow.xaml.cs
+++ Monitor/Windows/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Monitor
+namespace Monitor.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
