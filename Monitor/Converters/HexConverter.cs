3dac6c63a2f68ce3b7115e8314ca8a078595c3a7
tearth <tearthdev@gmail.com>
2020-04-06 18:49:09 +0200

    Add hex converter to WPF binding

---
+++ Monitor/Converters/HexConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Monitor.Converters
+{
+    public class HexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte)
+            {
+                return $"0x{value:x2}";
+            }
+            else if (value is ushort)
+            {
+                return $"0x{value:x4}";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueToParse = value.ToString().Replace("0x", "");
+            if (targetType == typeof(byte))
+            {
+                if (byte.TryParse(valueToParse, NumberStyles.HexNumber, null, out var result))
+                {
+                    return result;
+                }
+            }
+            else if (targetType == typeof(ushort))
+            {
+                if (ushort.TryParse(valueToParse, NumberStyles.HexNumber, null, out var result))
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+    }
+}
