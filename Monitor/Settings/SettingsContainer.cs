d207d5d0e1dbfff7b94cc1d58ed02686ae0e0d6f
tearth <tearthdev@gmail.com>
2020-04-08 18:52:26 +0200

    Add settings container

---
+++ Monitor/Settings/SettingsContainer.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Monitor.Settings
+{
+    public class SettingsContainer
+    {
+        public SettingsData Data { get; private set; }
+        private readonly string _filePath;
+
+        public SettingsContainer(string filePath)
+        {
+            _filePath = filePath;
+            Load();
+        }
+
+        public void Load()
+        {
+            if (File.Exists(_filePath))
+            {
+                var content = File.ReadAllText(_filePath);
+                Data = JsonConvert.DeserializeObject<SettingsData>(content);
+            }
+            else
+            {
+                Data = new SettingsData();
+            }
+        }
+
+        public void Save()
+        {
+            var content = JsonConvert.SerializeObject(Data);
+            File.WriteAllText(_filePath, content);
+        }
+    }
+}
---
+++ Monitor/Settings/SettingsData.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+namespace Monitor.Settings
+{
+    public class SettingsData : INotifyPropertyChanged
+    {
+        private string _path;
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                OnPropertyChanged("Path");
+            }
+        }
+
+        private string _arguments;
+        public string Arguments
+        {
+            get => _arguments;
+            set
+            {
+                _arguments = value;
+                OnPropertyChanged("Arguments");
+            }
+        }
+
+        private string _address;
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
