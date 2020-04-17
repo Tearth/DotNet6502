451a468f29c43b96aa72866a3478368ce07f94af
tearth <tearthdev@gmail.com>
2020-04-17 15:17:39 +0200

    Fix style in hex JSON converter

--- Monitor/Instructions/Converters/HexJsonConverter.cs
+++ Monitor/Instructions/Converters/HexJsonConverter.cs
@@ -20,10 +20,15 @@ namespace Monitor.Instructions.Converters
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType == JsonToken.Null)
+            {
                 return null;
+            }
+
             var token = JToken.Load(reader);
             if (!(token is JValue))
+            {
                 throw new JsonSerializationException("Token was not a primitive");
+            }
 
             return byte.Parse((string)token, NumberStyles.HexNumber);
         }
