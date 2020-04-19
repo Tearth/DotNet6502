6ab8f1f5d1a3bd7accf28b459615954d0763d9fb
tearth <tearthdev@gmail.com>
2020-04-19 15:12:19 +0200

    Add About window

---
+++ Monitor/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Monitor.Windows
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void GitHubLink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
