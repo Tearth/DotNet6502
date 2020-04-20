0fa88597e9d47c66e7dfc7a875d197c438e4b598
tearth <tearthdev@gmail.com>
2020-04-21 00:16:23 +0200

    Add SEC instruction

---
+++ CPU/InstructionDecode/Instructions/SecInstruction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CPU.Registers;
+
+namespace CPU.InstructionDecode.Instructions
+{
+    /// <summary>
+    /// SEt Carry
+    /// </summary>
+    public class SecInstruction : InstructionBase
+    {
+        public SecInstruction(ushort opCode, AddressingMode addressingMode, Mos6502Core core) : base("SEC", opCode, addressingMode, core)
+        {
+
+        }
+
+        /// <summary>
+        /// Length: 1, Cycles: 1F + 1.
+        /// </summary>
+        protected override void ExecuteInImplicitMode()
+        {
+            // 1 cycle
+            Core.Registers.ChangeFlag(StatusFlags.Carry, true);
+            Core.YieldCycle();
+        }
+    }
+}
