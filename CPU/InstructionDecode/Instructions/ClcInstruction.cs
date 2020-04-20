01e90614e03e268ebe8e517042ad3e8c0e86bb71
tearth <tearthdev@gmail.com>
2020-04-21 00:15:59 +0200

    Add CLC instruction

---
+++ CPU/InstructionDecode/Instructions/ClcInstruction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CPU.Registers;
+
+namespace CPU.InstructionDecode.Instructions
+{
+    /// <summary>
+    /// CLear Carry
+    /// </summary>
+    public class ClcInstruction : InstructionBase
+    {
+        public ClcInstruction(ushort opCode, AddressingMode addressingMode, Mos6502Core core) : base("CLC", opCode, addressingMode, core)
+        {
+
+        }
+
+        /// <summary>
+        /// Length: 1, Cycles: 1F + 1.
+        /// </summary>
+        protected override void ExecuteInImplicitMode()
+        {
+            // 1 cycle
+            Core.Registers.ChangeFlag(StatusFlags.Carry, false);
+            Core.YieldCycle();
+        }
+    }
+}
