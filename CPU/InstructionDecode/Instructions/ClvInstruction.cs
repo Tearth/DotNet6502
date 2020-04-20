c9c0ac4604976f8fa68dedec93d2e7a1d0e59333
tearth <tearthdev@gmail.com>
2020-04-21 00:16:17 +0200

    Add CLV instruction

---
+++ CPU/InstructionDecode/Instructions/ClvInstruction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CPU.Registers;
+
+namespace CPU.InstructionDecode.Instructions
+{
+    /// <summary>
+    /// CLear oVerflow
+    /// </summary>
+    public class ClvInstruction : InstructionBase
+    {
+        public ClvInstruction(ushort opCode, AddressingMode addressingMode, Mos6502Core core) : base("CLV", opCode, addressingMode, core)
+        {
+
+        }
+
+        /// <summary>
+        /// Length: 1, Cycles: 1F + 1.
+        /// </summary>
+        protected override void ExecuteInImplicitMode()
+        {
+            // 1 cycle
+            Core.Registers.ChangeFlag(StatusFlags.Overflow, false);
+            Core.YieldCycle();
+        }
+    }
+}
