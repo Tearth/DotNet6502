c7560e93624bb35dc5e4c351ce67a24f46e1c20b
tearth <tearthdev@gmail.com>
2020-04-21 00:16:05 +0200

    Add CLD instruction

---
+++ CPU/InstructionDecode/Instructions/CldInstruction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CPU.Registers;
+
+namespace CPU.InstructionDecode.Instructions
+{
+    /// <summary>
+    /// CLear Decimal
+    /// </summary>
+    public class CldInstruction : InstructionBase
+    {
+        public CldInstruction(ushort opCode, AddressingMode addressingMode, Mos6502Core core) : base("CLD", opCode, addressingMode, core)
+        {
+
+        }
+
+        /// <summary>
+        /// Length: 1, Cycles: 1F + 1.
+        /// </summary>
+        protected override void ExecuteInImplicitMode()
+        {
+            // 1 cycle
+            Core.Registers.ChangeFlag(StatusFlags.DecimalMode, false);
+            Core.YieldCycle();
+        }
+    }
+}
